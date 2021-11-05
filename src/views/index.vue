48e8c5635cff73f6c6cc11636ad4ec6e07464967
youguangsheng <1476600908@qq.com>
2021-11-05 11:11:05 +0800

    feat: 新增todo标题，todo输入框，todo路由初始化配置

---
+++ src/views/index.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="todos">
+    <div class="todos-title">todos</div>
+    <div class="todos-mask">
+      <div class="todos-body">
+        <todo-comp></todo-comp>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import TodoComp from "./todos/index.vue";
+export default defineComponent({
+  name: "Home",
+  components: { TodoComp }
+});
+</script>
+
+<style scoped lang="scss">
+.todos {
+  width: 100%;
+  height: 100%;
+  position: relative;
+
+  &-title {
+    color: rgb(231, 71, 43);
+    font-size: 100px;
+    font-weight: 700;
+    padding: 50px;
+    text-align: center;
+  }
+
+  &-body {
+    width: 600px;
+    margin: 200px auto;
+    z-index: 10;
+  }
+
+  &-mask {
+    position: absolute;
+    left: 0;
+    right: 0;
+    top: 0;
+    bottom: 0;
+    background-color: rgba(240, 240, 240, 0.8);
+  }
+}
+</style>
---
+++ src/views/todos/index.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="todo-container">
+    <header class="todo-header">
+      <input class="todo-input" placeholder="请输入" />
+    </header>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped lang="scss">
+.todo-container {
+  padding: 20px;
+  border-radius: 10px;
+  background: #fff;
+  position: relative;
+  box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.2), 0 25px 50px 0 rgba(0, 0, 0, 0.1);
+
+  input::-webkit-input-placeholder,
+  input::-moz-placeholder,
+  input:-ms-input-placeholder {
+    font-style: italic;
+    font-weight: 300;
+    color: rgba(0, 0, 0, 0.4);
+  }
+
+  input:focus {
+    outline: 0;
+  }
+
+  .todo-input {
+    font-style: italic;
+    width: 100%;
+    font-size: 22px;
+    padding: 16px 16px 16px 60px;
+    border: none;
+    box-shadow: inset 0 -2px 1px rgba(0, 0, 0, 0.03);
+  }
+}
+</style>
