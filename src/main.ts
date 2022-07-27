63b50714e184b87cef13e13f305a67346f3e3836
samco <ygs_it@163.com>
2022-07-27 16:12:19 +0800

    添加基本样式

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import router from "./router";
-import { createPinia } from "pinia";
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
+import './assets/scss/index.scss';
+import { createPinia } from 'pinia';
 
 createApp(App)
   .use(router)
   .use(createPinia())
-  .mount("#app");
+  .mount('#app');
